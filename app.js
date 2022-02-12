54379ccd870543d0366a7afbf0e79cff604a6ace
yusufstawan <yusufsetiyawan45@gmail.com>
2022-02-12 20:18:17 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const dark = document.querySelector(".dark");
+const night = document.querySelector(".night");
+const light = document.querySelector(".light");
+const sidebar = document.querySelector(".sidebar");
+const activeListItem = document.querySelector(".list-item.active");
+
+dark.addEventListener("click", () => {
+    sidebar.className = "sidebar";
+    activeListItem.className = "list-item active";
+});
+
+night.addEventListener("click", () => {
+    sidebar.className = "sidebar night";
+    activeListItem.className = "list-item night active";
+});
+
+light.addEventListener("click", () => {
+    sidebar.className = "sidebar light";
+    activeListItem.className = "list-item light active";
+});
