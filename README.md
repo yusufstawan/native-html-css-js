553919dc3472de689a7a28ab524964bdb16c39ef
yusufstawan <yusufsetiyawan45@gmail.com>
2022-02-13 21:17:25 +0700

    update readme

--- README.md
+++ README.md
@@ -2,5 +2,4 @@ Progress-bar-circle
 
 ![image](https://user-images.githubusercontent.com/28127002/153757203-0342a988-a750-400c-9dac-34cb6a211696.png)
 
-
 ![image](https://user-images.githubusercontent.com/28127002/153757132-b3b1d78b-d80f-42b3-adf9-9280878de859.png)
--- app.js
+++ app.js
@@ -1,36 +1,36 @@
 // Progress bar
-// const progress = document.querySelector('.progress');
-// const loading = document.querySelector('.loading');
-
-// let i = 0;
-// const fakeUploadPerc = [0, 10, 25, 40, 42, 60, 70, 75, 90, 100];
-
-// const interval = setInterval(() => {
-//     progress.style.width = `${fakeUploadPerc[i]}%`;
-//     loading.innerHTML = `${fakeUploadPerc[i]}%`;
-//     i++;
-//     if (i == fakeUploadPerc.length) {
-//         clearInterval(interval);
-//         loading.innerHTML = `Completed`;
-//     }
-// }, 1000);
-
-// Progress circle
-const circle = document.querySelector('.progress-circle')
+const progress = document.querySelector('.progress');
 const loading = document.querySelector('.loading');
 
 let i = 0;
 const fakeUploadPerc = [0, 10, 25, 40, 42, 60, 70, 75, 90, 100];
 
-const circumference = circle.getTotalLength();
-
 const interval = setInterval(() => {
-    circle.style.strokeDashoffset =
-        circumference - (fakeUploadPerc[i] / 100) * circumference;
+    progress.style.width = `${fakeUploadPerc[i]}%`;
     loading.innerHTML = `${fakeUploadPerc[i]}%`;
     i++;
     if (i == fakeUploadPerc.length) {
         clearInterval(interval);
-        loading.innerHTML = `Ok :)`;
+        loading.innerHTML = `Completed`;
     }
 }, 1000);
+
+// Progress circle
+// const circle = document.querySelector('.progress-circle')
+// const loading = document.querySelector('.loading');
+
+// let i = 0;
+// const fakeUploadPerc = [0, 10, 25, 40, 42, 60, 70, 75, 90, 100];
+
+// const circumference = circle.getTotalLength();
+
+// const interval = setInterval(() => {
+//     circle.style.strokeDashoffset =
+//         circumference - (fakeUploadPerc[i] / 100) * circumference;
+//     loading.innerHTML = `${fakeUploadPerc[i]}%`;
+//     i++;
+//     if (i == fakeUploadPerc.length) {
+//         clearInterval(interval);
+//         loading.innerHTML = `Ok :)`;
+//     }
+// }, 1000);
